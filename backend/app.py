fd994fbb8f9445e52fe64c2851db2ced4b79f87a
Alex Linden <alextl2004@gmail.com>
2025-06-13 11:06:38 -0700

    boilerplate for psql in app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, jsonify
 from models import db, User, Task  # Make sure models are correctly imported
+from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
-# Configuration settings (use SQLite for simplicity here)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todone.db'
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
 
 # Initialize the database
 db.init_app(app)
---
+++ backend/config.py
@@ -0,0 +1,1 @@
+# might keep in app.py
